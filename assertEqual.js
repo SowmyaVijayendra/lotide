c3a22e66498d2416eeda53ef2a0a153a1c7e4a26
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-03-24 21:05:57 -0400

    updating head.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  actual === expected ? console.log("🟢🟢🟢Assertion Passed: " + actual + " === " + expected) : console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+  actual === expected ? console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
