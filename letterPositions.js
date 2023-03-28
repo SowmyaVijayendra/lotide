659208baf89c18ad3e4f0fd267badca58f98e89d
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-03-28 07:33:12 -0400

    adding letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  let counter=0;
+  for(let letter of sentence){
+    if(letter !== ' '){
+    if(results[letter])
+      results[letter].push(counter);
+    else{
+      results[letter] = [counter];     
+      }
+    }
+    counter++;
+  }
+  return results;
+};
+
+function eqArrays(arr1, arr2) { 
+  if(!arr1 && !arr2)
+    return true;
+  else if(!arr1 || !arr2)
+    return false;
+  else if (arr1.length != arr2.length)
+    return false;
+  else{
+    let i=0;
+    while(i < arr1.length ){
+      if(arr1[i] !== arr2[i])
+        return false;
+      i++;
+    }
+    return true;
+  }
+}
+
+function assertArraysEqual(arr1, arr2){
+  if(eqArrays(arr1, arr2))
+    console.log(`🟢🟢🟢 ${arr1} and ${arr2} are equal`) 
+  else
+  console.log(`🛑🛑🛑 ${arr1} and ${arr2} are not equal`) 
+}
+
+const result = letterPositions("Hello");
+assertArraysEqual(result["l"], [2,3]);
+assertArraysEqual(result["H"], [0]);
+assertArraysEqual(result["o"], [4]);
